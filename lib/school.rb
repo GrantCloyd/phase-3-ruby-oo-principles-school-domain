474b237d9de3b403df5a63286beb3a96cb2fa871
Grant Cloyd <dgcloyd@gmail.com>
2021-06-29 16:40:19 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School
+attr_accessor :roster, :name
+
+def initialize(name)
+    @name = name
+   @roster = {}
+end
+
+def add_student(name, grade)
+    @roster[grade] ||= [] 
+    @roster[grade] << name 
+end
+
+def grade(level)
+ @roster[level]
+end
+
+def sort 
+ update = @roster.each {|k,v| v.sort! {|a,b| a <=> b } }
+end
+
+end
